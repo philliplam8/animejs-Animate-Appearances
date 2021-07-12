3c385a9959fd64c1f44e78047681633635efb898
philliplam8 <81057116+philliplam8@users.noreply.github.com>
2021-07-12 12:36:06 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+anime({
+    targets: '#animate',
+    
+    // Properties
+    scale: [0, 1],
+    borderRadius: {
+      value: 0,
+      duration: 1000
+    },
+  
+    duration: 800,
+    
+    easing: 'easeInOutQuart',
+    //Animation Parameters
+    direction: 'alternate',
+    loop: true
+  })
